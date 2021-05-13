3daa33bf012b292bd81a1121a975e320fb8eea76
Esha-Sherring <esherring2016@gmail.com>
2021-05-13 22:08:03 +0530

    google auth route added

--- server/controllers/authcontrollers.js
+++ server/controllers/authcontrollers.js
@@ -11,6 +11,52 @@
     const Steps = require('../models/steps');
 
 
+    //Route for google authentication of user
+    module.exports.gauth = async (req, res) => {
+      console.log('google authentication route');
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+      return res.status(400).send('error occured');
+      }
+      else
+      {
+              try{
+               
+                const {name,email,password,avatar,gender,phone } = req.body;
+                const user = await User.findOne({ email: email })
+                if (user) {
+              console.log("User already exists");
+              return res.status(500).send({ error: "user already exist" });
+              
+                    }
+                    function generateAccessToken(user) {
+                      return jwt.sign(user, process.env.TOKEN_SECRET);
+                    }
+                
+                const newUser = new User({
+                 
+                  name,
+                  email,
+                  password,
+                  avatar,
+                  gender,
+                  phone 
+                 
+                }) ;
+                console.log(newUser.id)
+                newUser.save();
+                const token = generateAccessToken({_id: newUser.id });
+              
+              
+                return res.json(token).status(200);
+              }catch(err){
+                console.log(err)
+                return res.status(404).send({ error: "server error" });
+              }
+              
+      }
+    }
+
     //Route to register User
     //matching passwords handled on the frontend
     module.exports.signup = async (req, res) => {
